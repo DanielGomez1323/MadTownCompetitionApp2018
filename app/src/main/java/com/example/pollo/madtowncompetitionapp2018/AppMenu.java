2ea511fe6a7e51f54c1c71913861965038f12af8
DanielGomez1323 <daniel_mgomez@yahoo.com>
2018-01-10 20:00:25 -0800

    Code for App Menu started
    Have to finish it.

--- app/src/main/java/com/example/pollo/madtowncompetitionapp2018/AppMenu.java
+++ app/src/main/java/com/example/pollo/madtowncompetitionapp2018/AppMenu.java
@@ -1,13 +1,30 @@
 package com.example.pollo.madtowncompetitionapp2018;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AppMenu extends AppCompatActivity {
+    EditText searchEditText;
+    Button searchButton;
+    Button scoutButton;
+    Button uploadButton;
+    Button teamsButton;
+    Button addPhotoButton;
+    Button viewPhotoButton;
+    Button importScheduleButton;
+    SQLiteDatabase myDB = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_menu);
+        searchEditText = findViewById(R.id.searchEditText);
+        searchButton = findViewById(R.id.searchButton);
+        scoutButton = findViewById(R.id.scoutButton);
+        uploadButton = findViewById(R.id.uploadButton);
+        
     }
 }
