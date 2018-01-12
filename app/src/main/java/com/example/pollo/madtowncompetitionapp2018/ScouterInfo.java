01eada84856015870a59722e271e3440f74a659e
DanielGomez1323 <daniel_mgomez@yahoo.com>
2018-01-11 19:25:08 -0800

    ScouterInfo code started.
    Basic code for the ScouterInfo started.

--- app/src/main/java/com/example/pollo/madtowncompetitionapp2018/ScouterInfo.java
+++ app/src/main/java/com/example/pollo/madtowncompetitionapp2018/ScouterInfo.java
@@ -2,12 +2,24 @@ package com.example.pollo.madtowncompetitionapp2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Checkable;
+import android.widget.EditText;
 
 public class ScouterInfo extends AppCompatActivity {
+    EditText scoutNameEdtiText;
+    CheckBox blueCheckBox;
+    CheckBox redCheckBox;
+    Button scoutMenuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scouter_info);
+        scoutNameEdtiText = findViewById(R.id.scoutNameEditText);
+        blueCheckBox = findViewById(R.id.blueCheckBox);
+        redCheckBox = findViewById(R.id.redCheckBox);
+        scoutMenuButton = findViewById(R.id.scoutMenuButton);
     }
 }
