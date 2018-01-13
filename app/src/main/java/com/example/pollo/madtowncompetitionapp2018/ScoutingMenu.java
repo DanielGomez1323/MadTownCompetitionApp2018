072b8cdc30631c0591f96dca1523fb4beac31581
DanielGomez1323 <daniel_mgomez@yahoo.com>
2018-01-13 12:09:09 -0800

    A few hiccups
    Forgot the ID's for the EditText.

--- app/src/main/java/com/example/pollo/madtowncompetitionapp2018/ScoutingMenu.java
+++ app/src/main/java/com/example/pollo/madtowncompetitionapp2018/ScoutingMenu.java
@@ -2,12 +2,23 @@ package com.example.pollo.madtowncompetitionapp2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ScoutingMenu extends AppCompatActivity {
+    AutoCompleteTextView teamNumberAutoEditText;
+    EditText matchNumberEditText;
+    Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scouting_menu);
+        teamNumberAutoEditText = findViewById(R.id.teamNumberAutoEditText);
+        matchNumberEditText = findViewById(R.id.matchNumberEditText);
+        startButton = findViewById(R.id.startButton);
+
+
     }
 }
