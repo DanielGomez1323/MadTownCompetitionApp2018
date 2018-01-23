f9b2ce30ff5bad83940c8b7108b4195ab3a423bd
DanielGomez1323 <daniel_mgomez@yahoo.com>
2018-01-22 18:18:59 -0800

    Sending and editing data.
    Seems like with the new update, the methods for sending data to the website have changed. Need to look more into HttpClient methods.

---
+++ app/src/main/java/com/example/pollo/madtowncompetitionapp2018/DataListAdapter.java
@@ -0,0 +1,105 @@
+package com.example.pollo.madtowncompetitionapp2018;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DataListAdapter extends CursorAdapter{
+    private LayoutInflater cursorInflater;
+    private Context mContext;
+
+    public DataListAdapter(Context context, Cursor c, int flags){
+        super(context, c, flags);
+        mContext = context;
+    }
+    public void bindView(View view, Context context, Cursor cursor){
+        TextView teamNumberTextView = view.findViewById(R.id.teamNumberTextView);
+        TextView matchNumberTextView = view.findViewById(R.id.matchNumberTextView);
+        TextView teamColorTextView = view.findViewById(R.id.teamColorTextView);
+        TextView robotPositionTextView = view.findViewById(R.id.robotPositionTextView);
+        TextView baseLineCrossedTextView = view.findViewById(R.id.baseLineCrossedTextView);
+        TextView autoHighTextView = view.findViewById(R.id.autoHighTextView);
+        TextView autoLowTextView = view.findViewById(R.id.autoLowTextView);
+        TextView highCubesTextView = view.findViewById(R.id.highCubesTextView);
+        TextView lowCubesTextView = view.findViewById(R.id.lowCubesTextView);
+        TextView vaultCubesTextView = view.findViewById(R.id.vaultPlacedTextView);
+        TextView robotClimbTextView = view.findViewById(R.id.robotClimbTextView);
+        TextView successTextView = view.findViewById(R.id.successTextView);
+        TextView robotNotesTextView = view.findViewById(R.id.robotNotesTextView);
+        teamNumberTextView.setText(teamNumberTextView.getText() + cursor.getString(cursor.getColumnIndex("teamNumber")));
+        matchNumberTextView.setText(matchNumberTextView.getText() + cursor.getString(cursor.getColumnIndex("matchNumber")));
+        robotPositionTextView.setText(robotPositionTextView.getText() + cursor.getString(cursor.getColumnIndex("robotPosition")));
+        baseLineCrossedTextView.setText(baseLineCrossedTextView.getText() + cursor.getString(cursor.getColumnIndex("baseLineCrossed")));
+        autoHighTextView.setText(autoHighTextView.getText() + cursor.getString(cursor.getColumnIndex("autoHighCubePlaced")));
+        autoLowTextView.setText(autoLowTextView.getText() + cursor.getString(cursor.getColumnIndex("autoLowCubePlaced")));
+        highCubesTextView.setText(highCubesTextView.getText() + cursor.getString(cursor.getColumnIndex("highCubesPlaced")));
+        lowCubesTextView.setText(lowCubesTextView.getText() + cursor.getString(cursor.getColumnIndex("lowCubesPlaced")));
+        vaultCubesTextView.setText(vaultCubesTextView.getText() + cursor.getString(cursor.getColumnIndex("vaultCubesPlaced")));
+        robotClimbTextView.setText(robotClimbTextView.getText() + cursor.getString(cursor.getColumnIndex("climbTime")));
+        successTextView.setText(successTextView.getText() + cursor.getString(cursor.getColumnIndex("climbSuccess")));
+        robotNotesTextView.setText(robotNotesTextView.getText() + cursor.getString(cursor.getColumnIndex("robotNotes")));
+        teamNumberTextView.setText("Team " + cursor.getString(cursor.getColumnIndex("teamNumber")));
+        matchNumberTextView.setText("Match " + cursor.getString(cursor.getColumnIndex("matchNumber")));
+        String color = cursor.getString(cursor.getColumnIndex("teamColor"));
+        switch(cursor.getString(cursor.getColumnIndex("teamColor"))){
+            case "1":
+                color = "Blue";
+                break;
+            case "0":
+                color = "Red";
+                break;
+            default:
+                break;
+        }
+        String robotPosition = cursor.getString(cursor.getColumnIndex("robotPosition"));
+        switch(cursor.getString(cursor.getColumnIndex("robotPosition"))){
+            case "0":
+                robotPosition = "Left Peg";
+                break;
+            case "1":
+                robotPosition = "Middle Peg";
+                break;
+            case "2":
+                robotPosition = "Right Peg";
+                break;
+            case "3":
+                robotPosition = "Did Not Show";
+            default:
+                break;
+        }
+        String baseLineCrossed = cursor.getString(cursor.getColumnIndex("baseLineCrossed"));
+        switch (cursor.getString(cursor.getColumnIndex("baseLineCrossed"))){
+            case "0":
+                baseLineCrossed = "Baseline Crossed";
+                break;
+            case "1":
+                baseLineCrossed = "Baseline Not Crossed";
+                break;
+            default:
+                break;
+        }
+        teamColorTextView.setText(color);
+        robotPositionTextView.setText(robotPosition);
+        baseLineCrossedTextView.setText(baseLineCrossed);
+        autoHighTextView.setText("Auto High Cube Placed: " + cursor.getString(cursor.getColumnIndex("autoHighCubePlaced")));
+        autoLowTextView.setText("Auto Low Cube Placed: " + cursor.getString(cursor.getColumnIndex("autoLowCubePlaced")));
+        highCubesTextView.setText("High Cubes Placed: " + cursor.getString(cursor.getColumnIndex("highCubesPlaced")));
+        lowCubesTextView.setText("Low Cubes Placed: " + cursor.getString(cursor.getColumnIndex("lowCubesPlaced")));
+        vaultCubesTextView.setText("Vault Cubes Placed: " + cursor.getString(cursor.getColumnIndex("vaultCubesPlaced")));
+        robotClimbTextView.setText("Climb Time: " + cursor.getString(cursor.getColumnIndex("climbTime")));
+        successTextView.setText("Successful Climb: " + cursor.getString(cursor.getColumnIndex("climbSuccess")));
+        robotNotesTextView.setText("Notes: " + cursor.getString(cursor.getColumnIndex("robotNotes")));
+    }
+
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        // R.layout.list_row is your xml layout for each row
+        cursorInflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = cursorInflater.inflate(R.layout.data_list, parent, false);
+        bindView(v, context, cursor);
+        return v;
+    }
+}
--- app/src/main/java/com/example/pollo/madtowncompetitionapp2018/UploadData.java
+++ app/src/main/java/com/example/pollo/madtowncompetitionapp2018/UploadData.java
@@ -1,15 +1,31 @@
 package com.example.pollo.madtowncompetitionapp2018;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class UploadData extends AppCompatActivity {
     SQLiteDatabase myDB = null;
@@ -20,9 +36,9 @@ public class UploadData extends AppCompatActivity {
     Button scrollUpButton;
     Match match;
     String query;
-    String address = "";
+    String address = "http://www.gorohi.com/1323/2017/data.php";
     String _id;
-    //dataListAdapter listAdapter;
+    DataListAdapter listAdapter;
 
     public class Match {
         String scoutName;
@@ -47,7 +63,7 @@ public class UploadData extends AppCompatActivity {
             Cursor c = myDB.rawQuery("SELECT * FROM PowerUp WHERE _id = " + String.valueOf(id), null);
             c.moveToFirst();
             if (c.getCount() > 0) {
-                scoutName = c.getString(c.getColumnIndex("scoutName"));
+                //scoutName = c.getString(c.getColumnIndex("scoutName"));
                 teamColor = c.getString(c.getColumnIndex("teamColor"));
                 teamNumber = c.getString(c.getColumnIndex("teamNumber"));
                 matchNumber = c.getString(c.getColumnIndex("matchNumber"));
@@ -60,7 +76,7 @@ public class UploadData extends AppCompatActivity {
                 vaultCubesPlaced = c.getString(c.getColumnIndex("vaultCubesPlaced"));
                 climbTime = c.getString(c.getColumnIndex("climbTime"));
                 climbSuccess = c.getString(c.getColumnIndex("climbSuccess"));
-                robotNotes = c.getString(c.getColumnIndex("robotNotes"));
+                robotNotes = c.getString(c.getColumnIndex("scoutName" + "robotNotes"));
                 Toast.makeText(getApplicationContext(), "Team " + teamNumber + ", Match " + matchNumber + " selected", Toast.LENGTH_SHORT).show();
             }
         }
@@ -78,7 +94,7 @@ public class UploadData extends AppCompatActivity {
         public static String toJSon(Match data) {
             try {
                 JSONObject object = new JSONObject();
-                object.put("scoutName", data.scoutName);
+                //object.put("scoutName", data.scoutName);
                 object.put("teamColor", data.teamColor);
                 object.put("teamNumber", data.teamNumber);
                 object.put("matchNumber", data.matchNumber);
@@ -91,7 +107,7 @@ public class UploadData extends AppCompatActivity {
                 object.put("vaultCubesPlaced", data.vaultCubesPlaced);
                 object.put("climbTime", data.climbTime);
                 object.put("climbSuccess", data.climbSuccess);
-                object.put("robotNotes", data.robotNotes);
+                object.put("robotNotes", data.robotNotes + data.scoutName);
                 return object.toString();
             } catch (Exception e) {
                 System.out.print(e);
@@ -111,5 +127,190 @@ public class UploadData extends AppCompatActivity {
         query = "SELECT * FROM PowerUp";
         Intent i = getIntent();
         String search = i.getStringExtra("search");
+        if (search != null) {
+            if (search.toLowerCase().contains("t")) {
+                search = search.replaceAll("[^\\d.]", "");
+                query = "SELECT * FROM PowerUp WHERE teamNumber = " + search;
+            } else if (search.toLowerCase().contains("m")) {
+                search = search.replaceAll("[^\\d.]", "");
+                query = "SELECT * FROM PowerUp WHERE matchNumber = " + search;
+            } else {
+                search = search.replaceAll("[^\\d.]", "");
+                query = "SELECT * FROM PowerUp WHERE teamNumber = " + search + " OR matchNumber = " + search;
+            }
+        }
+        myDB = openOrCreateDatabase("FRC", MODE_PRIVATE, null);
+
+        c = myDB.rawQuery(query, null);
+        c.moveToFirst();
+        if (c.getCount() > 0) {
+            try {
+                listAdapter = new DataListAdapter(UploadData.this, c, 0);
+                lv.setAdapter(listAdapter);
+                lv.setLongClickable(true);
+                lv.setSelection(listAdapter.getCount() - 1);
+                c.moveToPosition(listAdapter.getCount() - 1);
+                _id = c.getString(c.getColumnIndex("_id"));
+                match.loadDatabase(Integer.parseInt(_id));
+                lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Cursor cc = (Cursor) listAdapter.getItem(position);
+                        cc.moveToPosition(position);
+                        _id = cc.getString(cc.getColumnIndex("_id"));
+                        match.loadDatabase(Integer.parseInt(_id));
+                    }
+                });
+                lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+                                                   int pos, long id) {
+                        // TODO Auto-generated method stub
+                        Cursor cur = (Cursor) listAdapter.getItem(pos);
+                        cur.moveToPosition(pos);
+                        String editID = cur.getString(cur.getColumnIndex("_id"));
+                        Intent editIntent = new Intent(getApplicationContext(), EditData.class);
+                        editIntent.putExtra("ID", editID);
+                        startActivityForResult(editIntent, 1);
+                        return true;
+                    }
+                });
+            } catch (Exception e) {
+                Log.d("ERROR", e.toString());
+            }
+
+
+            uploadButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (match.isLoaded()) {
+                        String json = toJSon(match);
+                        if (json != null) {
+                            new MyAsyncTask().execute(json);
+                        }
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Hey, select a match man.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+        deleteButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                myDB = openOrCreateDatabase("FRC", MODE_PRIVATE, null);
+                myDB.execSQL("DELETE FROM SteamWorks WHERE _id = " + _id);
+                updateList();
+                return true;
+            }
+        });
+        scrollUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lv.smoothScrollToPosition(0);
+            }
+        });
+    }
+
+    private class MyAsyncTask extends AsyncTask<String, Integer, Double> {
+        String response = "";
+
+        @Override
+        protected Double doInBackground(String... params) {
+            postData(params[0]);
+            return null;
+        }
+
+        protected void onPostExecute(Double result) {
+            Toast.makeText(getApplicationContext(), response, Toast.LENGTH_SHORT).show();
+        }
+
+
+        public void postData(String jsonString) {
+            HttpsURLConnection httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(address);
+            InputStream inputStream = null;
+            try {
+                List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>(1);
+                nameValuePair.add(new BasicNameValuePair("data", jsonString));
+                try {
+                    httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair));
+
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                HttpResponse httpResponse = httpClient.execute(httpPost);
+                inputStream = httpResponse.getEntity().getContent();
+                if (inputStream != null) {
+                    response = convertInputStreamToString(inputStream);
+                } else {
+                    response = "Did not Work!";
+                }
+                inputStream.close();
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if(Integer.parseInt(android.os.Build.VERSION.SDK)> 5
+                && keyCode == KeyEvent.KEYCODE_BACK){
+            c.close();
+            myDB.close();
+
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyLongPress(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK)
+        {
+            c.close();
+            myDB.close();
+            Intent i = new Intent(this, ScoutingMenu.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+            return true;
+        }
+        return super.onKeyLongPress(keyCode, event);
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while((line = bufferedReader.readLine()) != null)
+        {
+            result += line;
+        }
+        inputStream.close();
+        return result;
+    }
+    public void updateList(){
+        myDB = openOrCreateDatabase("FRC", MODE_PRIVATE, null);
+        Cursor c2  = myDB.rawQuery(query, null);
+        listAdapter.changeCursor(c2);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+            if(resultCode == Activity.RESULT_OK){
+                String match = data.getStringExtra("MATCH_NUMBER");
+                String teamN = data.getStringExtra("TEAM_NUMBER");
+                Toast.makeText(getApplicationContext(),"Team " + teamN + ", Match " + match + "updated.", Toast.LENGTH_SHORT).show();
+                updateList();
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+
+            }
+        }
     }
 }
